5dbdbb468473a83bb2c98a6ccfa6bbc88aed65fa
nyckolasaraujo <nyckolasaraujo.93@outlook.com>
2024-08-27 12:58:50 -0300

    first commit

---
+++ javas.js
@@ -0,0 +1,22 @@
+
+
+const mybutton = document.querySelector(".number-one")
+const myform = document.querySelector(".formulario")
+
+
+
+
+function clique() {
+
+myform.style.left = "30%"
+myform.style.transform= "translateX(-50px)"
+
+
+}
+
+function form(){
+
+myform.style.left= "-430px"
+myform.style.transform= "translateX(0px)"
+
+}
